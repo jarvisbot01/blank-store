dcdc9b8e7b9c2b8dd1cb4d8fe28b22fc801d94ba
Jose Daniel Rodriguez <142543881+jarvisbot01@users.noreply.github.com>
2024-03-11 20:19:37 +0000

    :sparkles: feat: add passport service

--- src/config/config.js
+++ src/config/config.js
@@ -3,4 +3,7 @@ dotenv.config();
 
 export default {
   port: process.env.PORT || 9090,
+  secret_key: process.env.SECRET_KEY,
+  microsoft_client_id: process.env.MICROSOFT_CLIENT_ID,
+  microsoft_client_secret: process.env.MICROSOFT_CLIENT_SECRET,
 };
